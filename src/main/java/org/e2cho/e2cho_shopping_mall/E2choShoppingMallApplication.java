bc8f89a6c11482747d30fae9a083e6ee3a8e9eaf
Jeong SeongHun <2000tjdgns@naver.com>
2024-09-11 17:46:05 +0900

    feat : 프로젝트 생성

---
+++ src/main/java/org/e2cho/e2cho_shopping_mall/E2choShoppingMallApplication.java
@@ -0,0 +1,13 @@
+package org.e2cho.e2cho_shopping_mall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class E2choShoppingMallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(E2choShoppingMallApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/e2cho/e2cho_shopping_mall/E2choShoppingMallApplicationTests.java
@@ -0,0 +1,13 @@
+package org.e2cho.e2cho_shopping_mall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class E2choShoppingMallApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
